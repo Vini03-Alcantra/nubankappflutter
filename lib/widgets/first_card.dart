d32ccf7365db37ed12315ed88ef1c85ccc3723d3
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-10-02 23:07:18 -0300

    first_card ajustado

--- lib/widgets/first_card.dart
+++ lib/widgets/first_card.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class FirstCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var altura = MediaQuery.of(context).size.height;
+    var largura = MediaQuery.of(context).size.width;
     return ClipRRect(
       borderRadius: BorderRadius.circular(5),
       child: Column(
@@ -32,40 +34,42 @@ class FirstCard extends StatelessWidget {
                                           children: <Widget>[
                                             Icon(
                                               Icons.credit_card,
-                                              color: Colors.grey
+                                              color: Colors.grey,
+                                              size: 26,
                                             ),
-                                            SizedBox(width: 5),
+                                            SizedBox(width: largura * 0.012),
                                             Text(
                                               "Cartão de Crédito", 
                                               style: TextStyle(
-                                                color: Colors.grey,
-                                                fontSize: 13
+                                                color: Colors.black87,
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 16
                                               ),
                                             ), 
                                           ],
                                         ),
                                       ),
                                       Padding(
-                                        padding: const EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                                        padding: EdgeInsets.only(left: largura * 0.05, top: altura * 0.02, bottom: altura * 0.02),
                                         child: SizedBox(
                                           width: double.infinity,
                                           child: Column(
                                             crossAxisAlignment: CrossAxisAlignment.start,
                                             children: <Widget>[
                                               Text("fatura Atual", textAlign: TextAlign.start, 
-                                              style: TextStyle(color: Colors.teal, fontSize: 13, fontWeight: FontWeight.bold),
+                                              style: TextStyle(color: Colors.teal, fontSize: 16, fontWeight: FontWeight.bold),
                                               ),
                                               Text.rich(
                                                 TextSpan(  
                                                   text: "R\$", 
                                                   children: [
-                                                    TextSpan(text: "600", style: TextStyle(fontWeight: FontWeight.bold)),
+                                                    TextSpan(text: "600", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36)),
                                                     TextSpan(text: ",50")
                                                   ]),
                                                   textAlign: TextAlign.start,
                                                   style: TextStyle(
                                                     color: Colors.teal,
-                                                    fontSize: 28,                                                    
+                                                    fontSize: 36,                                                    
                                                   ),
                                               ), 
                                               Text.rich(
@@ -84,7 +88,7 @@ class FirstCard extends StatelessWidget {
                                                 textAlign: TextAlign.start,
                                                 style: TextStyle(
                                                   color: Colors.black,
-                                                  fontSize: 12
+                                                  fontSize: 16
                                                 ),
                                               )
                                             ],
@@ -102,7 +106,7 @@ class FirstCard extends StatelessWidget {
                     )
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 12, bottom: 12, left: 10, right: 15),
+                    padding: EdgeInsets.only(top: altura * 0.02, bottom: altura * 0.02, left: largura * 0.02, right: largura * 0.03),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(5),
                       child: Container(
@@ -128,7 +132,7 @@ class FirstCard extends StatelessWidget {
                             )
                           ],
                         ),
-                        width: 7,
+                        width: largura * 0.02,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(5)
                         ),
@@ -143,29 +147,30 @@ class FirstCard extends StatelessWidget {
             flex: 1,
             child: Container(
               child: Padding(
-                padding: const EdgeInsets.all(20),
+                padding: EdgeInsets.only(left: largura * 0.05, right: largura * 0.05, top: altura * 0.001, bottom: altura * 0.001),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Icon(  
                       Icons.add_shopping_cart,
-                      color: Colors.grey
+                      color: Colors.black54,
+                      size: 30,
                     ),
                     SizedBox(width: 5),
                     Flexible(
-                      child: Text("Compra mais recente no supermercado R\$34.56 quinta feir", 
-                      style: TextStyle(color: Colors.black, fontSize: 13),
+                      child: Text("Compra mais recente no supermercado R\$34.56 quinta feira", 
+                      style: TextStyle(color: Colors.black87, fontSize: 15),
                       )
                     ),
-                    Icon(
-                      Icons.chevron_right,
-                      color: Colors.grey[400],
-                      size: 18,
-                    )
+                    // Icon(
+                    //   Icons.chevron_right,
+                    //   color: Colors.grey[400],
+                    //   size: 18,
+                    // )
                   ],
                 ),
               ),
-              color: Colors.grey[200],
+              color: Colors.grey[300],
             ),
           )
         ],
